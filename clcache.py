frerich/clcache

commit ca1c51404e3835b2e9f754161071cce238c073e3

Fixed various errors.

clcache.py
@@ -246,7 +246,7 @@ def printTraceStatement(msg):
 def expandCommandLine(cmdline):
     ret = []
             
-    for arg in cmdline[1:]:
+    for arg in cmdline[0:]:
         if arg[0] == '@':
             includeFile = arg[1:]
             f = open(includeFile, 'r')
@@ -355,9 +355,9 @@ if not compiler:
     sys.exit(1)
 
 if "CLCACHE_DISABLE" in os.environ:
-    sys.exit(invokeRealCompiler(compiler)[0], sys.argv[1:])
+    sys.exit(invokeRealCompiler(compiler, sys.argv[1:]))
    
-cmdLine = expandCommandLine(sys.argv)
+cmdLine = expandCommandLine(sys.argv[1:])
 analysisResult, sourceFile, outputFile = analyzeCommandLine(cmdLine)
 
 cache = ObjectCache()
@@ -374,7 +374,7 @@ if analysisResult != AnalysisResult.Ok:
         printTraceStatement("Cannot cache invocation as %s: called for linking" % (' '.join(cmdLine)) )
         stats.registerCallForLinking()
     stats.save()
-    sys.exit(invokeRealCompiler(compiler)[0], cmdLine)
+    sys.exit(invokeRealCompiler(compiler, cmdLine))
 
 cachekey = cache.computeKey(compiler, cmdLine)
 if cache.hasEntry(cachekey):

commit 3ace677ed754aa9e467e21a4289f082cac56c276

Fixed grave bug when computing cache key
It turned out that when computing the cache key (and when invoking
the preprocessor!) we always droppd the first item from the given
'commandLine' argument. This got broken by commit
ca1c51404e3835b2e9f754161071cce238c073e3 which caused 'commandLine'
to only contain the arguments to the compiler, not the actual
compiler itself.

The cache key computation code still assumed that the first element
of the command line is the compiler binary, so it skipped that.

This bug not only caused some builds to break (because e.g. important
include paths were missing!), it also means that the first compiler
argument was not taken into consideration when computing the cache
key. So compiling the same soruce twice, but with slightly different
compiler invocations which only differ in their first argument, would
incorrectly result in a cache hit.

clcache.py
@@ -125,7 +125,7 @@ class ObjectCache:
 
     def computeKey(self, compilerBinary, commandLine):
         ppcmd = [compilerBinary, "/EP"]
-        ppcmd += [arg for arg in commandLine[1:] if not arg in ("-c", "/c")]
+        ppcmd += [arg for arg in commandLine if not arg in ("-c", "/c")]
         preprocessor = Popen(ppcmd, stdout=PIPE, stderr=PIPE)
         (preprocessedSourceCode, pperr) = preprocessor.communicate()
 
@@ -134,7 +134,7 @@ class ObjectCache:
             sys.stderr.write("clcache: preprocessor failed\n")
             sys.exit(preprocessor.returncode)
 
-        normalizedCmdLine = self._normalizedCommandLine(commandLine[1:])
+        normalizedCmdLine = self._normalizedCommandLine(commandLine)
 
         stat = os.stat(compilerBinary)
         sha = hashlib.sha1()