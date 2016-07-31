c6cbe080aba00d6ea4660ed21af8dfa0747e24bc
Frerich Raabe <raabe@froglogic.com>
2016-07-31 20:27:19 +0200

    Added integration test for CLCACHE_BASEDIR functionality
    This exercises the CLCACHE_BASEDIR feature a bit, which permits moving
    build directories around and having all direct hits still work because
    the paths to include files are done relative to some configurable base
    dir.

---
+++ tests/integrationtests/basedir/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+std::string databaseId() { return "opm"; }
+
+#endif // !defined(CONSTANTS_H)
---
+++ tests/integrationtests/basedir/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "constants.h"
+
+int main()
+{
+    std::cout << databaseId() << '\n';
+}
+
+
