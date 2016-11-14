ceab921e4750b58612be15110250824084772cbc
Frerich Raabe <raabe@froglogic.com>
2016-11-14 20:46:44 +0100

    Inlined call to postprocessUnusableManifestMiss() in processDirect()
    I now inlined all relevant code to be able to reorder it such that the
    code for handling cache misses is performed without holding any locks.

--- clcache.py
+++ clcache.py
@@ -1638,9 +1638,28 @@ def processDirect(cache, objectFile, compiler, cmdLine, sourceFile):
         else:
             unusableManifestMissReason = Statistics.registerSourceChangedMiss
 
-        return postprocessUnusableManifestMiss(
-            cache, objectFile, manifestSection, manifestHash, sourceFile, compiler, cmdLine,
-            unusableManifestMissReason)
+        stripIncludes = False
+        if '/showIncludes' not in cmdLine:
+            cmdLine = list(cmdLine)
+            cmdLine.insert(0, '/showIncludes')
+            stripIncludes = True
+        returnCode, compilerOutput, compilerStderr = invokeRealCompiler(compiler, cmdLine, captureOutput=True)
+        includePaths, compilerOutput = parseIncludesSet(compilerOutput, sourceFile, stripIncludes)
+
+        entry = createManifestEntry(manifestHash, includePaths)
+        cachekey = entry.objectHash
+
+        cleanupRequired = False
+        section = cache.compilerArtifactsRepository.section(cachekey)
+        with section.lock, cache.statistics.lock, cache.statistics as stats:
+            unusableManifestMissReason(stats)
+            if returnCode == 0 and os.path.exists(objectFile) and not section.hasEntry(cachekey):
+                artifacts = CompilerArtifacts(objectFile, compilerOutput, compilerStderr)
+                cleanupRequired = addObjectToCache(stats, cache, section, cachekey, artifacts)
+                manifest = createOrUpdateManifest(manifestSection, manifestHash, entry)
+                manifestSection.setManifest(manifestHash, manifest)
+
+        return returnCode, compilerOutput, compilerStderr, cleanupRequired
 
 def processNoDirect(cache, objectFile, compiler, cmdLine, environment):
     cachekey = CompilerArtifactsRepository.computeKeyNodirect(compiler, cmdLine, environment)
